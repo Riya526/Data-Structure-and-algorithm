838fb7d270572a3f39f6d30b5571854c71fc3cf8
Riya Verma <188327203+Riya526@users.noreply.github.com>
2026-08-26 23:15:49 +0530

    Time: 93 ms (9.67%), Space: 46.1 MB (15.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }if(n==1){
-            return 1;
+        HashMap<Integer,Integer> dp=new HashMap<>();
+        if(n==0||n==1){
+            return n;
         }
-        int ans1=fib(n-1);
-        int ans2=fib(n-2);
-        return ans1+ans2;
+        if(dp.containsKey(n)){
+            return dp.get(n);
+        }
+        int a1=fib(n-1);
+        int a2=fib(n-2);
+        int ans=a1+a2;
+        dp.put(n,ans);
+        return ans;
     }
 }
