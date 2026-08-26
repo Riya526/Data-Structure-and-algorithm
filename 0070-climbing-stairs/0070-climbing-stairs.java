934a4945886c928d1b4d8c8cfbdda7af8d82ac86
Riya Verma <188327203+Riya526@users.noreply.github.com>
2026-08-27 01:35:55 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    HashMap<Integer,Integer> dp=new HashMap<>();
+    public int climbStairs(int n) {
+        return fun(0,n);
+        
+    }
+    int fun(int i,int n){
+        
+        if(i==n){
+            return 1;
+        }if(i>n){
+            return 0;
+        }
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+        
+        int ans=fun(i+1,n)+fun(i+2,n);
+        dp.put(i,ans);
+        return ans;
+        
+    }
+}
